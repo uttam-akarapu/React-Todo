5c5f12cd70fc06d53c2f69081fb3301a7e26eced
Uttam <uttamadeveloper@gmail.com>
2020-05-30 19:43:52 +0530

    Updates

---
+++ precache-manifest.a89ac87bf54a632af3718d91fa8c89ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da872f1a33f3d1c6cd050569866bb4c",
+    "url": "/React-Todo/index.html"
+  },
+  {
+    "revision": "4e071d4511a6325276c1",
+    "url": "/React-Todo/static/css/main.5733c7f0.chunk.css"
+  },
+  {
+    "revision": "400614a7a1a1e8c411ac",
+    "url": "/React-Todo/static/js/2.e971d57b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todo/static/js/2.e971d57b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e071d4511a6325276c1",
+    "url": "/React-Todo/static/js/main.48a4f52f.chunk.js"
+  },
+  {
+    "revision": "4f2ec3fd6828965b6196",
+    "url": "/React-Todo/static/js/runtime-main.2c5090f4.js"
+  }
+]);
